05f43def22d3ec9a8c6a66e106f8aa98f0eed824
Harald Bregu <harald.bregu@gmail.com>
2020-08-11 12:10:46 +0200

    Initial commit

---
+++ app/src/main/java/it/ninespartans/dinamo/MainActivity.kt
@@ -0,0 +1,17 @@
+package it.ninespartans.dinamo
+
+import android.content.res.Resources
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        toolbar.title = "Dashboard"
+        setSupportActionBar(toolbar)
+    }
+
+}
